3d39e99c0ea20404b0bb49c0791a9b4e72d97b9c
tristanhoppe <tristan.hoppe@my.wheaton.edu>
2019-03-19 10:36:01 -0500

    deleted api_key_example, copy and modify configuration_example instead

---
+++ config/configuration_example.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * CREATE A COPY OF THIS NAMED configuration.php
+ * 
+ * 
+ * 
+ * Config file for variables in getData.php, collections.php, api_key.php
+ * 
+ * 
+ */
+return array(
+    'expireTime' => 120, //json_cache.php, Time limit(minutes) before a file has expire (currently 2 hours)
+    'collectionType' => 'groups', //getData.php, collections.php, groups or user
+    'groupID' => '2264127', //getData.php, collections.php, ID number
+    'api_key' => '' //api_key.php, api key
+)
+?>
